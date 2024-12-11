26894c6a2f1920f84842b5fb9a808b31288be29c
Nympfonic <arys.steam@gmail.com>
2024-12-11 20:02:20 +0000

    Added Patch Manager to automatically enable ModulePatches

---
+++ Utils/ModulePatchManager.cs
@@ -0,0 +1,39 @@
+﻿using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Donuts.Utils;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class DisablePatchAttribute : Attribute;
+
+public static class ModulePatchManager
+{
+	private static readonly Dictionary<Type, ModulePatch> _patches = [];
+
+	static ModulePatchManager()
+	{
+		IEnumerable<Type> patchTypes = Assembly.GetExecutingAssembly().GetTypes();
+		foreach (Type patch in patchTypes)
+		{
+			if (patch.BaseType == typeof(ModulePatch) &&
+				patch.GetCustomAttribute(typeof(DisablePatchAttribute)) == null)
+			{
+				_patches[patch] = (ModulePatch)Activator.CreateInstance(patch);
+			}
+		}
+	}
+	
+	public static void EnablePatches()
+	{
+		foreach (ModulePatch patch in _patches.Values)
+			patch.Enable();
+	}
+
+	public static void DisablePatches()
+	{
+		foreach (ModulePatch patch in _patches.Values)
+			patch.Disable();
+	}
+}
