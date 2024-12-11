9840e38282bdef8b2a45f72671e7d84c50ee72c9
Nympfonic <arys.steam@gmail.com>
2024-12-11 16:05:10 +0000

    Added Json.NET attributes to BotLimitPresets.cs

--- Models/BotLimitPresets.cs
+++ Models/BotLimitPresets.cs
@@ -1,16 +1,37 @@
-﻿namespace Donuts.Models
+﻿using Newtonsoft.Json;
+
+namespace Donuts.Models;
+
+[JsonObject]
+public class BotLimitPresets
 {
-	public class BotLimitPresets
-	{
-		public int FactoryBotLimit { get; set; }
-		public int InterchangeBotLimit { get; set; }
-		public int LaboratoryBotLimit { get; set; }
-		public int LighthouseBotLimit { get; set; }
-		public int ReserveBotLimit { get; set; }
-		public int ShorelineBotLimit { get; set; }
-		public int WoodsBotLimit { get; set; }
-		public int CustomsBotLimit { get; set; }
-		public int TarkovStreetsBotLimit { get; set; }
-		public int GroundZeroBotLimit { get; set; }
-	}
+	[JsonProperty("factoryBotLimit")]
+	public int FactoryBotLimit { get; set; }
+	
+	[JsonProperty("interchangeBotLimit")]
+	public int InterchangeBotLimit { get; set; }
+	
+	[JsonProperty("laboratoryBotLimit")]
+	public int LaboratoryBotLimit { get; set; }
+	
+	[JsonProperty("lighthouseBotLimit")]
+	public int LighthouseBotLimit { get; set; }
+	
+	[JsonProperty("reserveBotLimit")]
+	public int ReserveBotLimit { get; set; }
+	
+	[JsonProperty("shorelineBotLimit")]
+	public int ShorelineBotLimit { get; set; }
+	
+	[JsonProperty("woodsBotLimit")]
+	public int WoodsBotLimit { get; set; }
+	
+	[JsonProperty("customsBotLimit")]
+	public int CustomsBotLimit { get; set; }
+	
+	[JsonProperty("tarkovStreetsBotLimit")]
+	public int TarkovStreetsBotLimit { get; set; }
+	
+	[JsonProperty("groundZeroBotLimit")]
+	public int GroundZeroBotLimit { get; set; }
 }
