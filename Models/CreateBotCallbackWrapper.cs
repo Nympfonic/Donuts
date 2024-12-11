efa5e522a6f16549b9eee51dd2f1bcca919484af
Nympfonic <arys.steam@gmail.com>
2024-12-11 16:31:23 +0000

    Refactored CreateBotCallbackWrapper.cs

--- Models/CreateBotCallbackWrapper.cs
+++ Models/CreateBotCallbackWrapper.cs
@@ -1,56 +1,40 @@
 ﻿using Comfort.Common;
+using Donuts.Utils;
 using EFT;
 using HarmonyLib;
+using JetBrains.Annotations;
 using System;
 using System.Diagnostics;
 
-namespace Donuts.Models
+namespace Donuts.Models;
+
+// Wrapper around method_11 called after bot creation, so we can pass it the BotCreationDataClass data
+public class CreateBotCallbackWrapper([NotNull] BotCreationDataClass botData)
 {
-	// Wrapper around method_10 called after bot creation, so we can pass it the BotCreationDataClass data
-	public class CreateBotCallbackWrapper(BotCreationDataClass botData)
+	private static CreateBotCallbackDelegate _createBotCallbackDelegate;
+	private readonly Stopwatch _stopwatch = new();
+
+	private delegate void CreateBotCallbackDelegate([NotNull] BotOwner botOwner, [NotNull] BotCreationDataClass data,
+		[CanBeNull] Action<BotOwner> callback, bool shallBeGroup, [NotNull] Stopwatch stopwatch);
+
+	public void CreateBotCallback([NotNull] BotOwner bot)
 	{
-		private static CreateBotCallbackDelegate createBotCallbackDelegate;
-		private readonly Stopwatch stopwatch = new();
-
-		private delegate void CreateBotCallbackDelegate(BotOwner botOwner, BotCreationDataClass data,
-			Action<BotOwner> callback, bool shallBeGroup, Stopwatch stopwatch);
-
-		public void CreateBotCallback(BotOwner bot)
+		BotSpawner botSpawner = Singleton<IBotGame>.Instance.BotsController.BotSpawner;
+		if (botSpawner == null)
 		{
-			var botSpawner = Singleton<IBotGame>.Instance.BotsController.BotSpawner;
-			if (botSpawner == null)
-			{
-				ClearDelegate();
-				return;
-			}
-
-			if (createBotCallbackDelegate == null || createBotCallbackDelegate.Target != botSpawner)
-			{
-				SetDelegate(botSpawner);
-			}
-
-			bool shallBeGroup = botData.SpawnParams?.ShallBeGroup != null;
-
-			// I have no idea why BSG passes a stopwatch into this call...
-			// TODO: Looks like SPT 3.10 has a patch that will remove BSG's instantiation of a stopwatch
-			// Remove for 3.10 update
-			stopwatch.Start();
-			//ReflectionCache.BotSpawner_method10_Method.Invoke(botSpawner, [bot, botData, null, shallBeGroup, stopWatch]);
-			createBotCallbackDelegate(bot, botData, null, shallBeGroup, stopwatch);
+			_createBotCallbackDelegate = null;
+			return;
 		}
 
-		private static void SetDelegate(BotSpawner botSpawner)
+		if (_createBotCallbackDelegate == null || _createBotCallbackDelegate.Target != botSpawner)
 		{
-			createBotCallbackDelegate = AccessTools.MethodDelegate<CreateBotCallbackDelegate>(
-				ReflectionCache.BotSpawner_method10_Method,
-				botSpawner,
-				false
-			);
+			_createBotCallbackDelegate = AccessTools.MethodDelegate<CreateBotCallbackDelegate>(
+				ReflectionHelper.BotSpawner_method11_Method, botSpawner, false);
 		}
 
-		private static void ClearDelegate()
-		{
-			createBotCallbackDelegate = null;
-		}
+		bool shallBeGroup = botData.SpawnParams?.ShallBeGroup != null;
+		
+		//ReflectionCache.BotSpawner_method11_Method.Invoke(botSpawner, [bot, botData, null, shallBeGroup, stopWatch]);
+		_createBotCallbackDelegate(bot, botData, null, shallBeGroup, _stopwatch);
 	}
 }
