6db8ec86f60b1d9f0eba63d053ea4ca7796fbfcf
Nympfonic <arys.steam@gmail.com>
2024-12-11 17:41:54 +0000

    Cleaned up PrepBotInfo.cs

--- Models/PrepBotInfo.cs
+++ Models/PrepBotInfo.cs
@@ -1,15 +1,20 @@
 ﻿using EFT;
 
-namespace Donuts.Models
+namespace Donuts.Models;
+
+public class PrepBotInfo(
+	WildSpawnType spawnType,
+	BotDifficulty difficulty,
+	EPlayerSide side,
+	bool isGroup = false,
+	int groupSize = 1)
 {
-	public class PrepBotInfo(WildSpawnType spawnType, BotDifficulty difficulty, EPlayerSide side,
-		bool isGroup = false, int groupSize = 1)
-	{
-		public WildSpawnType SpawnType { get; set; } = spawnType;
-		public BotDifficulty Difficulty { get; set; } = difficulty;
-		public EPlayerSide Side { get; set; } = side;
-		public BotCreationDataClass Bots { get; set; }
-		public bool IsGroup { get; set; } = isGroup;
-		public int GroupSize { get; set; } = groupSize;
-	}
+	public WildSpawnType SpawnType { get; } = spawnType;
+	public BotDifficulty Difficulty { get; } = difficulty;
+	public EPlayerSide Side { get; } = side;
+	public BotCreationDataClass Bots { get; set; }
+	public bool IsGroup { get; } = isGroup;
+	public int GroupSize { get; } = groupSize;
+	
+	
 }
