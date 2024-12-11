d5daf573fc6787a0a629d01f118682b49189213e
Nympfonic <arys.steam@gmail.com>
2024-12-11 16:15:24 +0000

    Updated BotGroupAddEnemyPatch.cs to use file-scoped namespace

--- Patches/BotGroupAddEnemyPatch.cs
+++ Patches/BotGroupAddEnemyPatch.cs
@@ -6,52 +6,51 @@ using System.Reflection;
 
 // Stolen from DanW's Questing Bots, thanks Dan!
 
-namespace Donuts.Patches
+namespace Donuts.Patches;
+
+internal class BotGroupAddEnemyPatch : ModulePatch
 {
-	internal class BotGroupAddEnemyPatch : ModulePatch
+	protected override MethodBase GetTargetMethod()
 	{
-		protected override MethodBase GetTargetMethod()
+		return AccessTools.Method(typeof(BotsGroup), nameof(BotsGroup.AddEnemy));
+	}
+
+	[PatchPrefix]
+	private static bool PatchPrefix(BotsGroup __instance, IPlayer person, EBotEnemyCause cause)
+	{
+		// Don't add invalid enemies
+		if (person == null || (person.IsAI && person.AIData?.BotOwner?.GetPlayer == null))
 		{
-			return AccessTools.Method(typeof(BotsGroup), nameof(BotsGroup.AddEnemy));
+			return false;
 		}
 
-		[PatchPrefix]
-		private static bool PatchPrefix(BotsGroup __instance, IPlayer person, EBotEnemyCause cause)
+		// We only care about bot groups adding you as an enemy
+		if (!person.IsYourPlayer)
 		{
-			// Don't add invalid enemies
-			if (person == null || (person.IsAI && person.AIData?.BotOwner?.GetPlayer == null))
-			{
-				return false;
-			}
-
-			// We only care about bot groups adding you as an enemy
-			if (!person.IsYourPlayer)
-			{
-				return true;
-			}
-
-			// This only matters in Scav raids
-			// TODO: This might also matter in PMC raids if a mod adds groups that are friendly to the player
-			//if (!Aki.SinglePlayer.Utils.InRaid.RaidChangesUtil.IsScavRaid)
-			//{
-			//	return true;
-			//}
-
-			// We only care about one enemy cause
-			if (cause != EBotEnemyCause.pmcBossKill)
-			{
-				return true;
-			}
-
-			// Get the ID's of all group members
-			List<BotOwner> groupMemberList = [];
-			for (int m = 0; m < __instance.MembersCount; m++)
-			{
-				groupMemberList.Add(__instance.Member(m));
-			}
-			//string[] groupMemberIDs = groupMemberList.Select(m => m.Profile.Id).ToArray();
-
 			return true;
 		}
+
+		// This only matters in Scav raids
+		// TODO: This might also matter in PMC raids if a mod adds groups that are friendly to the player
+		//if (!Aki.SinglePlayer.Utils.InRaid.RaidChangesUtil.IsScavRaid)
+		//{
+		//	return true;
+		//}
+
+		// We only care about one enemy cause
+		if (cause != EBotEnemyCause.pmcBossKill)
+		{
+			return true;
+		}
+
+		// Get the ID's of all group members
+		List<BotOwner> groupMemberList = [];
+		for (int m = 0; m < __instance.MembersCount; m++)
+		{
+			groupMemberList.Add(__instance.Member(m));
+		}
+		//string[] groupMemberIDs = groupMemberList.Select(m => m.Profile.Id).ToArray();
+
+		return true;
 	}
 }
