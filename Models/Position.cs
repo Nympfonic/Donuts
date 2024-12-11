86096cfeb63b9ef0304e823cdc69d23f08d4b7f4
Nympfonic <arys.steam@gmail.com>
2024-12-11 17:41:32 +0000

    Refactored Position.cs

--- Models/Position.cs
+++ Models/Position.cs
@@ -1,24 +1,25 @@
 ﻿using Newtonsoft.Json;
 using UnityEngine;
 
-namespace Donuts.Models
+namespace Donuts.Models;
+
+[JsonObject]
+public class Position
 {
-	public class Position
+	private Vector3 _internalVector = Vector3.positiveInfinity;
+
+	[JsonProperty("x")]
+	public float x { get; set; }
+
+	[JsonProperty("y")]
+	public float y { get; set; }
+
+	[JsonProperty("z")]
+	public float z { get; set; }
+
+	public Vector3 ToVector3()
 	{
-		[JsonIgnore]
-		private Vector3 internalVector = Vector3.positiveInfinity;
-
-		public float x { get; set; }
-		public float y { get; set; }
-		public float z { get; set; }
-
-		public Vector3 ToVector3()
-		{
-			if (internalVector == Vector3.positiveInfinity)
-			{
-				internalVector = new Vector3(x, y, z);
-			}
-			return internalVector;
-		}
+		_internalVector.Set(x, y, z);
+		return _internalVector;
 	}
 }
