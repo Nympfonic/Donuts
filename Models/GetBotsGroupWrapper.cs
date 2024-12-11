ca9ceec686d2e10a689135e8264c6d7c27fab225
Nympfonic <arys.steam@gmail.com>
2024-12-11 16:23:07 +0000

    Minor touches to GetBotsGroupWrapper.cs

--- Models/GetBotsGroupWrapper.cs
+++ Models/GetBotsGroupWrapper.cs
@@ -1,24 +1,22 @@
 ﻿using EFT;
 
-namespace Donuts.Models
+namespace Donuts.Models;
+
+/// <summary>
+/// Custom <see cref="BotSpawner.GetGroupAndSetEnemies"/> wrapper that handles grouping bots into multiple groups within the same bot zone.
+/// </summary>
+public class GetBotsGroupWrapper(BotSpawner botSpawner)
 {
-	/// <summary>
-	/// Custom <see cref="BotSpawner.GetGroupAndSetEnemies"/> wrapper that handles grouping bots into multiple groups within the same botzone.
-	/// </summary>
-	public class GetBotsGroupWrapper(BotSpawner botSpawner)
+	private BotsGroup _group;
+
+	public BotsGroup GetGroupAndSetEnemies(BotOwner bot, BotZone zone)
 	{
-		private BotsGroup group;
-
-		public BotsGroup GetGroupAndSetEnemies(BotOwner bot, BotZone zone)
+		// If we haven't found/created our BotsGroup yet, do so, and then lock it so nobody else can use it
+		if (_group == null)
 		{
-			// If we haven't found/created our BotsGroup yet, do so, and then lock it so nobody else can use it
-			if (group == null)
-			{
-				group = botSpawner.GetGroupAndSetEnemies(bot, zone);
-				group.Lock();
-			}
-
-			return group;
+			_group = botSpawner.GetGroupAndSetEnemies(bot, zone);
+			_group.Lock();
 		}
+		return _group;
 	}
 }
