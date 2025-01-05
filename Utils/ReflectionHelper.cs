5cc5de633de22eb0ec76493d26eda57590017ed2
Nympfonic <arys.steam@gmail.com>
2025-01-05 04:55:25 +0000

    Fixed broken reflection method

--- Utils/ReflectionHelper.cs
+++ Utils/ReflectionHelper.cs
@@ -11,5 +11,5 @@ internal static class ReflectionHelper
 	//internal static readonly FieldInfo BotSpawner_cancellationTokenSource_Field = AccessTools.Field(typeof(BotSpawner), "_cancellationTokenSource");
 
 	//internal static readonly MethodInfo BotSpawner_method9_Method = AccessTools.Method(typeof(BotSpawner), "method_9");
-	internal static readonly MethodInfo BotSpawner_method11_Method = AccessTools.Method(typeof(BotSpawner), "method_10");
+	internal static readonly MethodInfo BotSpawner_method11_Method = AccessTools.Method(typeof(BotSpawner), "method_11");
 }
