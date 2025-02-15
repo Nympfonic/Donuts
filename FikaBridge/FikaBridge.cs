92714740721831a9a52d50c7d7bf24ae7aa5ae5b
Arys <arys.steam@gmail.com>
2025-02-15 23:29:33 +0000

    Added Fika bridge project for future features

---
+++ FikaBridge/FikaBridge.cs
@@ -0,0 +1,25 @@
+using Comfort.Common;
+using Fika.Core.Coop.Utils;
+using Fika.Core.Networking;
+
+namespace Donuts.FikaBridge;
+
+public static class FikaBridge
+{
+	private static LineOfSightController _lineOfSightController;
+	private static PlayerLineOfSightChecker _playerLineOfSightChecker;
+	
+	public static void Initialize()
+	{
+		if (FikaBackendUtils.IsServer)
+		{
+			_lineOfSightController = new LineOfSightController(Singleton<FikaServer>.Instance);
+			Singleton<LineOfSightController>.Create(_lineOfSightController);
+		}
+		else
+		{
+			_playerLineOfSightChecker = new PlayerLineOfSightChecker(Singleton<FikaClient>.Instance);
+			Singleton<PlayerLineOfSightChecker>.Create(_playerLineOfSightChecker);
+		}
+	}
+}
